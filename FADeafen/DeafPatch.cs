b79232364d330234ee06f9c9daa7f6d1cb9ffcc7
Zachary Braden <103508025+kobrakon@users.noreply.github.com>
2023-01-01 02:54:33 -0500

    implement workaround for my own fix

--- FADeafen/DeafPatch.cs
+++ FADeafen/DeafPatch.cs
@@ -1,53 +1,38 @@
+using EFT;
 using System;
+using UnityEngine;
+using System.Linq;
 using Comfort.Common;
 using System.Reflection;
-using EFT;
 using EFT.InventoryLogic;
 using System.Threading.Tasks;
 using Aki.Reflection.Patching;
-using UnityEngine;
+
 
 namespace ahhmyears
 {
     internal struct PlayerInfo
     {
         internal static GameWorld gameWorld
-        {
-            get
-            {
-                return Singleton<GameWorld>.Instance;
-            }
-        }
+        { get { return Singleton<GameWorld>.Instance; } }
 
         internal static Player.FirearmController FC
-        {
-            get
-            {
-                return player.HandsController as Player.FirearmController;
-            }
-        }
+        { get { return player.HandsController as Player.FirearmController; } }
 
         internal static Player player
-        {
-            get
-            {
-                return gameWorld.AllPlayers[0];
-            }
-        }
+        { get { return gameWorld.AllPlayers[0]; } }
 
-        internal static bool PlayerHasEarPro() => player.Profile.Inventory.Equipment.GetSlot(EquipmentSlot.Earpiece).ContainedItem != null;
+        internal static bool PlayerHasEarPro() => player.Profile.Inventory.Equipment.GetSlot(EquipmentSlot.Earpiece).ContainedItem != null || player.Profile.Inventory.Equipment.GetSlot(EquipmentSlot.Headwear).ContainedItem != null && (player.Profile.Inventory.Equipment.GetSlot(EquipmentSlot.Headwear).ContainedItem as LootItemClass).Slots.Any(item => item.ContainedItem != null && item.ContainedItem.GetItemComponent<SlotBlockerComponent>() != null && !item.ContainedItem.GetItemComponent<SlotBlockerComponent>().ConflictingSlotNames.Contains("Earpiece")) || player.Profile.Inventory.Equipment.GetSlot(EquipmentSlot.Headwear).ContainedItem != null && player.Profile.Inventory.Equipment.GetSlot(EquipmentSlot.Headwear).ContainedItem.GetItemComponent<SlotBlockerComponent>() != null && player.Profile.Inventory.Equipment.GetSlot(EquipmentSlot.Headwear).ContainedItem.GetItemComponent<SlotBlockerComponent>().ConflictingSlotNames.Contains("Earpiece");
     }
 
     public class OuchiePatch : ModulePatch
     {
-        protected override MethodBase GetTargetMethod()
-        {
-            return typeof(Player.FirearmController).GetMethod("RegisterShot", BindingFlags.Instance | BindingFlags.NonPublic);
-        }
+        protected override MethodBase GetTargetMethod() => typeof(Player.FirearmController).GetMethod("RegisterShot", BindingFlags.Instance | BindingFlags.NonPublic);
 
         [PatchPostfix]
         static void PostFix(Player.FirearmController __instance)
         {
+            if (PlayerInfo.player is HideoutPlayer) return; // hideout player has no health controller
             if (PlayerInfo.FC == __instance && GoodToDeafen()) { DoEarOuchie(false); } else if (TargetGoodToDeafen(__instance)) DoEarOuchie(true);
         }
 
@@ -61,19 +46,23 @@ namespace ahhmyears
             {
                 try
                 {
+                    PlayerInfo.player.ActiveHealthController.DoStun(1, 0);
                     PlayerInfo.player.ActiveHealthController.DoContusion(4, 50);
                 } catch (Exception e)
                 {
-                    Plugin.logger.LogError("Attempting to access ActiveHealthController resulted in an exception, falling back to PlayerHealthControlller\n" + e);
+                    Plugin.logger.LogError("Attempting to access ActiveHealthController resulted in an exception, falling back to PlayerHealthControlller" + e);
+                    PlayerInfo.player.PlayerHealthController.DoStun(1, 0);
                     PlayerInfo.player.PlayerHealthController.DoContusion(4, 100);
                 }
             }
             try
             {
+                PlayerInfo.player.ActiveHealthController.DoStun(1, 0);
                 PlayerInfo.player.ActiveHealthController.DoContusion(0, 100);
             } catch (Exception e)
             {
-                Plugin.logger.LogError("Attempting to access ActiveHealthController resulted in an exception, falling back to PlayerHealthControlller\n" + e);
+                Plugin.logger.LogError("Attempting to access ActiveHealthController resulted in an exception, falling back to PlayerHealthControlller" + e);
+                PlayerInfo.player.PlayerHealthController.DoStun(1, 0);
                 PlayerInfo.player.PlayerHealthController.DoContusion(0, 100);
             }
         }
@@ -90,7 +79,11 @@ namespace ahhmyears
         static void PreFix(Grenade __instance)
         {
             float dist = Vector3.Distance(__instance.transform.position, PlayerInfo.player.Transform.position);
-            if (!PlayerInfo.PlayerHasEarPro() && dist <= 50) PlayerInfo.player.ActiveHealthController.DoContusion(100 / (dist / 2), dist * 0.50f);
+            if (!PlayerInfo.PlayerHasEarPro() && dist <= 30)
+            {
+                PlayerInfo.player.ActiveHealthController.DoStun(1, 0);
+                PlayerInfo.player.ActiveHealthController.DoContusion(30 / (dist / 2), 100 / dist);
+            }
         }
     }
 }
